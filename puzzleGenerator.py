4aa3dd1b8413cf044875c3f7ab2e2493ccb9b6e1
Jomana Shaltout <166835339+jomana2006@users.noreply.github.com>
2026-06-28 16:30:43 +0300

    Update puzzleGenerator.py
    Adding lists into file

--- puzzleGenerator.py
+++ puzzleGenerator.py
@@ -1,3 +1,16 @@
 import random
 import string
 
+Directions = [
+     (0 , 1), #right direction
+     (0 , -1), #left direction
+     (1 , 0), #down direction
+     (-1 , 0), #up direction
+     (1 , 1), #down-right diagonal direction
+     (-1 , -1), #up-left diagonal direction
+     (1 , -1), #down-left diagonal direction
+     (-1 , 1) #up-right diagonal direction
+]
+
+Letters = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ") #list of letters to fill empty spaces on the grid
+

03579009ac773a03a23039617c218055c6afe86a
Jomana Shaltout <166835339+jomana2006@users.noreply.github.com>
2026-06-28 20:21:15 +0300

    Update puzzleGenerator.py
    generating random row, column, and direction to start filling in word

--- puzzleGenerator.py
+++ puzzleGenerator.py
@@ -14,9 +14,21 @@ Directions = [
 
 Letters = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ") #list of letters to fill empty spaces on the grid
 
-def generatePuzzleGrid(words) {
+def generatePuzzleGrid(words):
      rows, cols = 15, 15
      grid = [[' ' for _ in range(cols)] for _ in range(rows)]
      
-     
-}
+     for word in words:
+          placed = False
+          while (not placed):
+               row = random.randint(0, 14) # I generate a random row value from index 0 to 14
+               col = random.randint(0, 14) # I generate a random column value from index 0 to 14
+               dr, dc = random.choice(Directions) # I generate a random direction value as (dr, dc) from the list Directions
+               for letter in range(len(word)): #I loop through every letter in the word from the words list in the GUI file (or a temporary testing list in this file)
+                    currentRow = row + (letter * dr)
+                    currentColumn = col + (letter * dc)
+                    if 0 <= currentRow < 15 and 0 <= currentColumn < 15:  #to avoid index errors
+                         if grid[currentRow][currentColumn] == ' ': # checking if the grid is empty
+                              grid[currentRow][currentColumn] = word[letter] # filling up the randomised chosen space with the letter from the word
+                              placed =  True # telling the loop that it is finished
+                              
